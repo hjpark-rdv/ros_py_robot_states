4d9ba9bace1048f07b97cfcba87d067705263e55
hjpark <hjpark@rendezvue.ai>
2020-09-29 05:38:51 +0000

    path planning constraints 기능 테스트

--- pick_n_place.py
+++ pick_n_place.py
@@ -4,7 +4,7 @@ import sys
 import copy
 import rospy
 import moveit_commander
-from moveit_msgs.msg import MoveGroupActionResult
+from moveit_msgs.msg import MoveGroupActionResult, RobotState, Constraints, OrientationConstraint
 import geometry_msgs.msg
 import shape_msgs.msg
 from math import pi
@@ -99,108 +99,29 @@ class PickNPlaceTutorial():
         pos_values = self.robot_group.get_current_pose()
         print pos_values
     def go_cup_ready(self):
-        move_group = self.robot_group
-        pose_goal = geometry_msgs.msg.Pose()
-        pose_goal.orientation.w = 0.697164469105
-        pose_goal.orientation.x = 0.716618448965
-        pose_goal.orientation.y = -0.0185623033006
-        pose_goal.orientation.z = -0.00866847830944
 
-        pose_goal.position.x = -0.213759464511
-        pose_goal.position.y = -0.473728179994
-        pose_goal.position.z = 0.899694597225
-
-        move_group.set_pose_target(pose_goal)
-        move_group.go()    
-        move_group = self.robot_group
-        pose_goal = geometry_msgs.msg.Pose()
-        pose_goal.orientation.w = 0.697164469105
-        pose_goal.orientation.x = 0.716618448965
-        pose_goal.orientation.y = -0.0185623033006
-        pose_goal.orientation.z = -0.00866847830944
-
-        pose_goal.position.x = -0.213759464511
-        pose_goal.position.y = -0.473728179994
-        pose_goal.position.z = 0.899694597225
-
-        move_group.set_pose_target(pose_goal)
-        move_group.go()
+        joint_goal = [1.4966198335851, -1.326799297366056,
+         -2.3646593370644577, -0.04153883619746379, 
+         2.091894065899511, 1.56272327949214]
+        self.jmove_to_pose_goal(joint_goal)
 
     def go_cup_pick(self):
-        move_group = self.robot_group
-        pose_goal = geometry_msgs.msg.Pose()
-        pose_goal.orientation.w = 0.697164469105
-        pose_goal.orientation.x = 0.716618448965
-        pose_goal.orientation.y = -0.0185623033006
-        pose_goal.orientation.z = -0.00866847830944
-
-        pose_goal.position.x = -0.213759464511
-        pose_goal.position.y = -0.593728179994
-        pose_goal.position.z = 0.899694597225
-
-        move_group.set_pose_target(pose_goal)
-        move_group.go()    
+        joint_goal = [1.5204237004310464, -1.2234713661491383, -2.060448448441854, -0.012849023353027792, 1.6853771921882825, 1.581778165977047]
+        self.jmove_to_pose_goal(joint_goal)
     def go_cup_pick_up(self):
-        move_group = self.robot_group
-        pose_goal = geometry_msgs.msg.Pose()
-        pose_goal.orientation.w = 0.697164469105
-        pose_goal.orientation.x = 0.716618448965
-        pose_goal.orientation.y = -0.0185623033006
-        pose_goal.orientation.z = -0.00866847830944
-
-        pose_goal.position.x = -0.213759464511
-        pose_goal.position.y = -0.593728179994
-        pose_goal.position.z = 0.959694597225
-
-        move_group.set_pose_target(pose_goal)
-        move_group.go()            
+        joint_goal = [1.5213124031006353, -1.0696250320846978, -2.13191968131102, -0.010384709049366059, 1.601077789316958, 1.5834105228422943]
+        self.jmove_to_pose_goal(joint_goal)
     def go_cup_pick_up_back(self):
-        move_group = self.robot_group
-        pose_goal = geometry_msgs.msg.Pose()
-        
-        pose_goal.orientation.w = 0.696290026114
-        pose_goal.orientation.x = 0.717493706632
-        pose_goal.orientation.y = -0.018006197871
-        pose_goal.orientation.z = -0.00766533208544
-        
-
-        pose_goal.position.x = -0.213033016781
-        pose_goal.position.y = -0.433996235451
-        pose_goal.position.z = 0.9593263894
-
-        move_group.set_pose_target(pose_goal)
-        move_group.go()                    
+        joint_goal = [1.485013394059338, -1.2205087459196038, -2.558914482811422, -0.05925392810520602, 2.176512843699474, 1.5491935569119]
+        self.jmove_to_pose_goal(joint_goal)              
     
     def go_cup_juice_plate_1(self):
-        move_group = self.robot_group
-        pose_goal = geometry_msgs.msg.Pose()
-        
-        pose_goal.orientation.x = 0.4832957098
-        pose_goal.orientation.y = -0.504994165185
-        pose_goal.orientation.z = -0.502877654077
-        pose_goal.orientation.w = 0.508448832282
-
-        pose_goal.position.x = -0.397201939325
-        pose_goal.position.y = -0.198214268682
-        pose_goal.position.z = 0.936391917232
-
-        move_group.set_pose_target(pose_goal)
-        move_group.go()  
+        joint_goal = [1.1585109273853935, -1.1412947340268882, -1.9651534712829661, 1.174693853054754, 1.5802267648312642, 1.5647516568196982]
+        self.jmove_to_pose_goal(joint_goal)              
+    
     def go_cup_juice_plate_2(self):
-        move_group = self.robot_group
-        pose_goal = geometry_msgs.msg.Pose()
-        
-        pose_goal.orientation.x = 0.483213559137
-        pose_goal.orientation.y = -0.505158316969
-        pose_goal.orientation.z = -0.502849044308
-        pose_goal.orientation.w = 0.508392141659
-
-        pose_goal.position.x = -0.518667834037
-        pose_goal.position.y = -0.196999708132
-        pose_goal.position.z = 0.907416595201
-
-        move_group.set_pose_target(pose_goal)
-        move_group.go()                    
+        joint_goal = [0.92240650967898, -1.172250392101962, -1.778228708394378, 0.9456193887305044, 1.4795156069155557, 1.4431953051824347]
+        self.jmove_to_pose_goal(joint_goal)                         
     
     def go_gripper_pos(self,pos):
         cup_hold = Int32MultiArray()
@@ -245,7 +166,10 @@ class PickNPlaceTutorial():
 
         move_group.set_pose_target(pose_goal)
         move_group.go()
-
+    def go_orange_pick_pos(self):
+        joint_goal = [-1.525941365018604, -0.14172073526193607, -0.9297368925373561, 0.0006108652381979773, -2.083661327493378, 1.5812857556003057]
+        self.jmove_to_pose_goal(joint_goal)              
+    
     def go_orange_to_juice_pot1(self):
         move_group = self.robot_group
         pose_goal = geometry_msgs.msg.Pose()
@@ -261,6 +185,74 @@ class PickNPlaceTutorial():
 
         move_group.set_pose_target(pose_goal)
         move_group.go()
+    def go_juicer_pick_cup_ok(self):
+        joint_goal = [1.1844676968659216, -1.2657127735462559, -1.8955148341283938, 1.187522023056912, 1.601077789316958, 1.4844723419921242]
+        self.jmove_to_pose_goal(joint_goal)   
+
+    def test_constraints(self):
+
+
+        constraint = Constraints()
+        constraint.name = "tilt constraint"
+        ocm = OrientationConstraint()
+        # 'base_link' is equal to the world link
+        ocm.header.frame_id = "world"
+        # The link that must be oriented upwards
+        ocm.link_name = "tcp"
+        # ocm.orientation = Quaternion(0.5, 0.5, 0.5, 0.5)
+        ocm.orientation.x =  0.683951325248
+        ocm.orientation.y =  -0.209429903965
+        ocm.orientation.z =  -0.202265206016
+        ocm.orientation.w =  0.668908429049
+
+        # Allow rotation of 45 degrees around the x and y axis
+        ocm.absolute_x_axis_tolerance = 0.5 #Allow max rotation of 45 degrees
+        ocm.absolute_y_axis_tolerance = 1.5 #Allow max rotation of 360 degrees
+        ocm.absolute_z_axis_tolerance = 0.5 #Allow max rotation of 45 degrees
+        # The tilt constraint is the only constraint
+        ocm.weight = 1
+        constraint.orientation_constraints = [ocm]
+
+        move_group = self.robot_group
+        pose_goal = geometry_msgs.msg.Pose()
+        
+        pose_goal.orientation.x = 0.683951325248
+        pose_goal.orientation.y = -0.209429903965
+        pose_goal.orientation.z = -0.202265206016
+        pose_goal.orientation.w = 0.668908429049
+
+        pose_goal.position.x = -0.461741097024
+        pose_goal.position.y = -0.433851093412
+        pose_goal.position.z = 1.26149858128
+        move_group.set_pose_target(pose_goal)
+
+        move_group.set_path_constraints(constraint)
+        move_group.plan()
+       
+
+    def test_constraints2(self):
+        move_group = self.robot_group
+        move_group.clear_path_constraints()
+        self.go_juicer_pick_cup_ok()
+        self.test_constraints()
+        # self.go_sealing_step1()
+    def go_juicer_constraint_test(self):
+
+        
+        move_group = self.robot_group
+        pose_goal = geometry_msgs.msg.Pose()
+        
+        pose_goal.orientation.x = 0.683951325248
+        pose_goal.orientation.y = -0.209429903965
+        pose_goal.orientation.z = -0.202265206016
+        pose_goal.orientation.w = 0.668908429049
+
+        pose_goal.position.x = -0.461741097024
+        pose_goal.position.y = -0.433851093412
+        pose_goal.position.z = 1.26149858128
+        move_group.set_pose_target(pose_goal)
+        move_group.go()
+
     def go_sealing_step1(self):
         move_group = self.robot_group
         pose_goal = geometry_msgs.msg.Pose()
@@ -365,7 +357,7 @@ def main():
                 pnp.go_cup_ready()
             elif( input == '2'):
                 pnp.go_cup_pick()
-                pnp.go_cup_hold()
+                pnp.go_gripper_pos(130)
                 pnp.go_cup_pick_up()
                 pnp.go_cup_pick_up_back()
             elif( input == '3'):
@@ -373,12 +365,16 @@ def main():
                 pnp.go_cup_juice_plate_2()
                 pnp.go_gripper_pos(0)
                 pnp.go_cup_juice_plate_1()
+            elif( input == '3.1'):
+                pnp.go_orange_pick_pos()
             elif( input == '4'):
                 pnp.go_orange_to_juice_pot1()                
                 pnp.go_orange_to_juice_pot2()
                 pnp.go_cup_release()
                 time.sleep(0.5)
-                pnp.go_orange_to_juice_pot1()                
+                pnp.go_orange_to_juice_pot1()             
+            elif( input == '4.1'):
+                pnp.go_juicer_pick_cup_ok()
             elif( input == '5'):
                 pnp.go_sealing_step1()
             elif( input == '6'):
@@ -412,6 +408,10 @@ def main():
                 pnp.go_testing_pos_move()
             elif( input == 'q'):
                 break
+            elif( input == 'a1'):
+                pnp.test_constraints()
+            elif( input == 'a2'):
+                pnp.test_constraints2()
             else:
                 pnp.show_current_joint()
                 pnp.show_current_pos()
